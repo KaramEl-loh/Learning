11ffa10b1caca0a10789ed543982ddecfd3b0eff
El-loh <kelloh@MGC12Z83Z9LVCG.fbpld77.ford.com>
2020-04-03 15:24:35 -0400

    added multidimensional internal representation examples

---
+++ RxJava/LogRxOperators/app/src/main/java/com/ford/logrxoperators/MultiDimensionalOperatorInternalRepresentation.kt
@@ -0,0 +1,96 @@
+package com.ford.logrxoperators
+
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
+
+
+fun main() {
+
+//    mappingObservableAToObservableBWithoutFlattening()
+//    mappingObservableAToObservableBUsingFlatMap()
+//    mappingObservableAToObservableBUsingMapAndMerge()
+//    mappingObservableAToObservableBUsingConcatMap()
+//    mappingObservableAToObservableBUsingMapAndConcat()
+//    mappingObservableAToObservableBUsingSwitchMap()
+//    mappingObservableAToObservableBUsingMapAndSwitchOnNext()
+
+}
+
+fun mappingObservableAToObservableBWithoutFlattening() {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+    obsA.map { obsB }.blockingSubscribe { println(it) }
+
+
+}
+
+fun mappingObservableAToObservableBUsingFlatMap() {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+    obsA.flatMap { obsB }.blockingSubscribe { println(it) }
+
+}
+
+fun mappingObservableAToObservableBUsingMapAndMerge() {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+
+    Observable.merge(obsA.map { obsB }).blockingSubscribe { println(it) }
+
+}
+
+fun mappingObservableAToObservableBUsingConcatMap() {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+    obsA.concatMap { obsB }.blockingSubscribe { println(it) }
+
+}
+
+fun mappingObservableAToObservableBUsingMapAndConcat() {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+
+    Observable.concat(obsA.map { obsB }).blockingSubscribe { println(it) }
+
+}
+
+fun mappingObservableAToObservableBUsingSwitchMap() {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+    obsA.switchMap { obsB }.blockingSubscribe { println(it) }
+
+}
+
+fun mappingObservableAToObservableBUsingMapAndSwitchOnNext() {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+
+    Observable.switchOnNext(obsA.map { obsB }).blockingSubscribe { println(it) }
+
+}
+
+fun qwe(): Observable<Long> {
+
+    val obsA = Observable.interval(1000, TimeUnit.MILLISECONDS).take(2)
+    val obsB = Observable.interval(500, TimeUnit.MILLISECONDS).take(5)
+
+    return obsA.flatMap { obsB }
+
+
+}
+
+
